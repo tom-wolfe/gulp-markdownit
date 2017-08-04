db41d29e5d02acc2ffc9c2f372b27206f39ceae4
Tom Wolfe <trwolfe13@gmail.com>
2017-08-04 20:05:25 +0100

    Add another test.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -10,9 +10,9 @@ const createFile = (contents) => {
   })
 }
 
-describe('gulp-markdownit', function () {
+describe('gulp-markdownit', () => {
   it('should render markdown if a buffer is provided.', function (done) {
-    var task = markdownIt()
+    const task = markdownIt()
     task.write(createFile(Buffer.from('# Heading')))
     task.once('data', function (file) {
       assert(file.isBuffer())
@@ -20,8 +20,16 @@ describe('gulp-markdownit', function () {
       done()
     })
   })
-  it('errors when a stream is provided', function (done) {
-    var task = markdownIt()
+  it('should pass over an empty file.', () => {
+    const task = markdownIt()
+    task.write(createFile(null))
+    task.once('data', function (file) {
+      assert.equal(file.contents, null)
+      done()
+    })
+  })
+  it('should error when a stream is provided', (done) => {
+    const task = markdownIt()
     assert.throws(() => {
       task.write(createFile(eventStream.readArray(['# Heading'])))
     })
